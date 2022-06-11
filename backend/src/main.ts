4f74038ed667fedaf88e9b0d6de17bd0dbec146c
Jerald <jeraldssw@gmail.com>
2022-06-11 23:52:56 +0800

    feat: init firebase module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ConfigService } from "@nestjs/config";
+import { ValidationPipe } from "@nestjs/common";
+import * as firebase from "firebase-admin";
+import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    const configService: ConfigService = app.get(ConfigService);
+    const serviceAccount: firebase.ServiceAccount = {
+        projectId: configService.get<string>("FIREBASE_PROJECT_ID"),
+        privateKey: configService.get<string>("FIREBASE_PRIVATE_KEY").replace(/\\n/g, "\n"),
+        clientEmail: configService.get<string>("FIREBASE_CLIENT_EMAIL"),
+    };
+
+    firebase.initializeApp({
+        credential: firebase.credential.cert(serviceAccount),
+    });
+
+    app.setGlobalPrefix("api");
+    app.enableCors();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
