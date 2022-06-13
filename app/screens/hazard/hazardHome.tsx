84d490117f16fa73ae02f50a7d4f8218a94dd814
yvonnelhs <yvonnefghijk@gmail.com>
2022-06-14 02:12:14 +0800

    hazard home: style changes and clean up

--- app/screens/hazard/hazardHome.tsx
+++ app/screens/hazard/hazardHome.tsx
@@ -4,53 +4,37 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const HazardHomepage = ({ navigation }: TScreenProp) => {
+    type ReportOptionProps = {
+        imageSource: {},
+        title:string,
+    }
+    const ReportOption = (props:ReportOptionProps) => {
+        return (
+            <TouchableOpacity containerStyle={{ width: "100%" }} onPress={() => navigation.push("HazardReporting")}>
+            <View style={styles.singleOption}>
+                <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+                    <Image style = {{width: 25, height: 25, resizeMode: "contain"}} source={props.imageSource} />
+                    <Text style={{ marginLeft: 10, fontFamily: "Poppins-Normal", fontSize: 15 }}>{props.title}</Text>
+                </View>
+                <Icon name="arrow-right" size={22} />
+            </View>
+        </TouchableOpacity>
+        )
+    }
+
     return (
         <View style={styles.container}>
             <Text style={{ fontFamily: "Poppins-SemiBold", fontSize: 25 }}>
                 Open Report
             </Text>
-            <Text style={{ fontFamily: "Poppins-SemiBold", fontSize: 15, alignSelf: "flex-start", marginTop: 30 }}>
+            <Text style={{ fontFamily: "Poppins-SemiBold", fontSize: 18, alignSelf: "flex-start", marginTop: 30 }}>
                 Area of concern
             </Text>
-            <TouchableOpacity containerStyle={{ width: "100%" }} onPress={() => navigation.push("HazardReporting")}>
-                <View style={styles.singleOption}>
-                    <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-                        <Image source={require("../../assets/hazard/hazard.png")} />
-                        <Text style={{ marginLeft: 10, fontFamily: "Poppins-Normal", fontSize: 15 }}>Hazard Reporting</Text>
-                    </View>
-                    <Icon name="arrow-right" size={20} />
-
-                </View>
-            </TouchableOpacity>
-            <TouchableOpacity containerStyle={{ width: "100%" }} onPress={() => navigation.push("SafetyConcern")}>
-
-                <View style={styles.singleOption}>
-                    <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-                        <Image source={require("../../assets/hazard/safety.png")} />
-                        <Text style={{ marginLeft: 10, fontFamily: "Poppins-Normal", fontSize: 15 }}>Safety concern</Text>
-                    </View>
-                    <Icon name="arrow-right" size={20} />
-
-
-                </View>
-            </TouchableOpacity>
-            <TouchableOpacity containerStyle={{ width: "100%" }} onPress={() => navigation.push("NearMiss")}>
-
-                <View style={styles.singleOption}>
-                    <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }} >
-                        <Image source={require("../../assets/hazard/near-miss.png")} />
-                        <Text style={{ marginLeft: 10, fontFamily: "Poppins-Normal", fontSize: 15 }}>Near miss</Text>
-
-                    </View>
-                    <Icon name="arrow-right" size={20} />
-                </View>
-            </TouchableOpacity>
+            <ReportOption imageSource={require("../../assets/hazard/hazard.png")} title = "Hazard Reporting" style = {{width: 23, height: 20}}/>
+            <ReportOption imageSource={require("../../assets/hazard/safety.png")} title = "Safety concern"/>
+            <ReportOption imageSource={require("../../assets/hazard/near-miss.png")} title = "Near miss"/>
         </View>
-
-
     );
-
-
 }
 
 export default HazardHomepage
@@ -65,8 +49,8 @@ const styles = StyleSheet.create({
     },
     singleOption: {
         width: "100%",
-        marginTop: 10,
-        padding: 15,
+        marginTop: 15,
+        padding: 18,
         shadowColor: "#000000",
         shadowOpacity: 0.2,
         borderRadius: 10,
