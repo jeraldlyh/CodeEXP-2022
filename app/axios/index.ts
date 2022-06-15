a1c765ad3c0accfa8d09de7e40da1a0fbec9f20b
yvonnelhs <yvonnefghijk@gmail.com>
2022-06-15 09:44:55 +0800

    change url

--- app/axios/index.ts
+++ app/axios/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://192.168.1.3:8000/api";
+const baseURL = "https://codeexp-2022.herokuapp.com/api";
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
